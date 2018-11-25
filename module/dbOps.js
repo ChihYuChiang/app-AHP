7b1938073f65a9d40ac3fceaebc915b73a2af1b2
ChihYuChiang <ckccyu@gmail.com>
2018-11-26 00:21:51 +0800

    Add: getCompare from db

--- module/dbOps.js
+++ module/dbOps.js
@@ -1,25 +1,47 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
-const cred = yaml.safeLoad(fs.readFileSync('./ref/credential.yml', 'utf8'));
+const assert = require('assert').strict;
 const MongoClient = require('mongodb').MongoClient;
+const CRED = yaml.safeLoad(fs.readFileSync('./ref/credential.yml', 'utf8'));
+const CONFIG = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
 
 
-const uri = cred.MangoAtlas.uri;
+const uri = CRED.MangoAtlas.uri;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-exports.postCompare = async function () {
+exports.postCompare = async function(data) {
   try {
     await client.connect();
     console.log("Connected correctly to server");
-    const collection = client.db("app").collection("test"); //ahp
+    const collection = client.db(CONFIG.MongoAtlas.db).collection(CONFIG.MongoAtlas.collection);
 
     //Insert a single document
-    let res = await collection.insertOne({a:1});
-    console.log(res.insertedCount)
-    console.log(res.insertedId)
+    let res = await collection.insertOne(data);
+    assert.equal(res.insertedCount, 1);
 
     //Close connection
     client.close();
+
+    return res.insertedId;
+  } catch(err) {
+    console.log(err.stack);
+  }
+}
+
+exports.getCompare = async function(recordId) {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const collection = client.db(CONFIG.MongoAtlas.db).collection(CONFIG.MongoAtlas.collection);
+    
+    //Retrieve a single doc by id
+    let res = await collection.findOne({ id: recordId });
+    console.log(res);
+
+    //Close connection
+    client.close();
+
+    return res;
   } catch(err) {
     console.log(err.stack);
   }
--- route/api.js
+++ route/api.js
@@ -7,15 +7,17 @@ const dbOps = require('../module/dbOps');
 //api/
 router.get('/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
-
-  // dbOps.postCompare();
 });
 
 router.post('/world', (req, res) => {
-  console.log(req.body)
   res.send(
     `I received your POST request. This is what you sent me: ${req.body}`,
   );
+
+  // dbOps.postCompare(req.body).then(() => {
+  //   dbOps.getCompare("5bfa9fe98e4a664d14a69f10");
+  // });
+  dbOps.postCompare(req.body)
 });
 
 
